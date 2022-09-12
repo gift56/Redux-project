57c9346ce320835c5a07aa3ec2f0bdd15d0a4af5
gift56 <efeasiughu@gmail.com>
2022-09-12 21:48:42 +0100

    imported header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <div className="ui fixed menu">
+      <div className="ui container center">
+        <h2>FreeShop</h2>
+      </div>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
