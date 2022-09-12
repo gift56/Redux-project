0bc3db0c66502b485f790c3ac8f03c3398aa7bbd
gift56 <efeasiughu@gmail.com>
2022-09-12 21:35:42 +0100

    created alot of files

---
+++ src/containers/redux/reducers/productReducer.js
@@ -0,0 +1,19 @@
+const initialState = {
+  products: [
+    {
+      id: 1,
+      title: "Gifted Will",
+      category: "programmer",
+    },
+  ],
+};
+
+export const productReducer = (state = initialState, { type, payload }) => {
+  switch (type) {
+    case ActionTypes.SET_PRODUCTS:
+      return state;
+
+    default:
+      return state;
+  }
+};
