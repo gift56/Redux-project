16aed7ab3e2dee9fc83ebbddd6cac23ee9164d01
gift56 <efeasiughu@gmail.com>
2022-09-18 15:56:44 +0100

    added some updates

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,8 +1,13 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 const Products = () => {
+  const products = useSelector((state) => state);
+
+  console.log(products);
+
   return (
-    <div>
+    <div className="ui grid container">
       <h1>Products</h1>
     </div>
   );
