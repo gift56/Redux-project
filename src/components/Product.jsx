08fbb2bc3becceb16fcd16358844a4ce71916830
gift56 <efeasiughu@gmail.com>
2022-09-18 18:56:39 +0100

    fetching datas

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -2,10 +2,21 @@ import React from "react";
 import { useSelector } from "react-redux";
 
 const Product = () => {
-  const products = useSelector((state) => state);
+  const products = useSelector((state) => state.allProducts.products);
+
+  const { id, title } = products[0];
+
+
   return (
     <div className="four column wide">
-      <div className="ui link cards"></div>
+      <div className="ui link cards">
+        <div className="card">
+          <div className="image"></div>
+          <div className="content">
+            <div className="header">{title}</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
+import axios from "axios";
 import { useSelector } from "react-redux";
 import Product from "./Product";
 
 const Products = () => {
   const products = useSelector((state) => state);
+
+  const fetchAllProducts = async () => {
+    const res = await axios
+      .get("https://fakestoreapi.com/products")
+      .catch((err) => {
+        console.log("errors: ", err);
+      });
+    console.log(res);
+  };
+
+  useEffect(() => {
+    fetchAllProducts();
+  }, []);
+
+  console.log(products);
 
   return (
     <div className="ui grid container">
